4f542e95cf212fa88f0c651172c60dd3449f8136
cbmarcum <carl.marcum@codebuilders.net>
2019-07-14 13:25:44 -0400

    Refs #71 - update to spring-security-core based on grails 3.3 upgrade. big changes to domain and addition of a listener instead of auto-injecting a service into the user domian. build.gradle - updated app minor version. index.adoc - some updates for past work. still needs more... added listener and updated spring conf.

---
+++ src/main/groovy/net/codebuilders/mybusiness/SecUserPasswordEncoderListener.groovy
@@ -0,0 +1,39 @@
+package net.codebuilders.mybusiness
+
+import grails.plugin.springsecurity.SpringSecurityService
+import org.grails.datastore.mapping.engine.event.AbstractPersistenceEvent
+import org.grails.datastore.mapping.engine.event.PreInsertEvent
+import org.grails.datastore.mapping.engine.event.PreUpdateEvent
+import org.springframework.beans.factory.annotation.Autowired
+import grails.events.annotation.gorm.Listener
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class SecUserPasswordEncoderListener {
+
+    @Autowired
+    SpringSecurityService springSecurityService
+
+    @Listener(SecUser)
+    void onPreInsertEvent(PreInsertEvent event) {
+        encodePasswordForEvent(event)
+    }
+
+    @Listener(SecUser)
+    void onPreUpdateEvent(PreUpdateEvent event) {
+        encodePasswordForEvent(event)
+    }
+
+    private void encodePasswordForEvent(AbstractPersistenceEvent event) {
+        if (event.entityObject instanceof SecUser) {
+            SecUser u = event.entityObject as SecUser
+            if (u.password && ((event instanceof  PreInsertEvent) || (event instanceof PreUpdateEvent && u.isDirty('password')))) {
+                event.getEntityAccess().setProperty('password', encodePassword(u.password))
+            }
+        }
+    }
+
+    private String encodePassword(String password) {
+        springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
+    }
+}
