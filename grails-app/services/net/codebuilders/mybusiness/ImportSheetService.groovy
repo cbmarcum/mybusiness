aab98d9fee8690327b2d415a5a60ea976c200850
cbmarcum <carl.marcum@codebuilders.net>
2019-11-19 20:08:18 -0500

    Refs #82 - fixed license headers

--- grails-app/services/net/codebuilders/mybusiness/ImportSheetService.groovy
+++ grails-app/services/net/codebuilders/mybusiness/ImportSheetService.groovy
@@ -1,7 +1,5 @@
-/*
- * *************************************************************
- *
- * Copyright 2018 Code Builders, LLC
+/* 
+ * Copyright 2016-2020 Code Builders, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * *************************************************************
+ * 
  */
 
 package net.codebuilders.mybusiness
